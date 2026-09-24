581d5c2fecd50a7a4fcfc68a768caabc1a4b7e07
Shirshak shrestha <shresthashirshak38@gmail.com>
2026-09-24 16:02:16 +0545

    Calculator

---
+++ basic programs revision/basic calculator/calculator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Num1: ";
+    cin>>a;
+    cout<<"Num2: ";
+    cin>>b;
+    int choice;
+    cout<<"Enter 1 for addition\n";
+    cout<<"Enter 2 for subtractoin\n";
+    cout<<"Enter 3 for multiplication\n";
+    cout<<"Enter 4 for division\n";
+    cout<<"Enter 0 to exit\n";
+    
+}
